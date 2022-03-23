802e2b4c313da1dc891450ff915268b6050230e0
Risalahsabila <risalahsabilawirna@gmail.com>
2022-03-23 16:34:56 +0700

    Source Code

---
+++ app/Controllers/Biodata.php
@@ -0,0 +1,15 @@
+<?php
+namespace App\Controllers;
+class Biodata extends BaseController
+{
+    public function index()
+    {
+        echo "<h1> Haii Perkenalkan</h1>";
+        echo "Nama Saya Risalah Sabila Wirna,
+              Saya biasa di panggil Risa, Risalah, Icha
+              Saya asli Jakarta dan saya tinggal di daerah Kalideres Jakarta Barat
+              Saya memiliki berat badan 56Kg dengan tinggi badan 168Cm
+              dan Saya menyukai olahraga Bulutangkis
+              Sekian Terimakasih salam kenal...";
+    }
+}
